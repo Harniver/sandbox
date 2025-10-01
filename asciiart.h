291b03bd95643e1ae3b1f2e2b41b45be499f559c
alefavv <favaroalessandro45345@gmail.com>
2025-10-01 16:37:50 +0200

    Update asciiart.h

--- asciiart.h
+++ asciiart.h
@@ -64,6 +64,26 @@ void kirby(){
     printf("⠈⠒⠀⠘⠊⠀⠀⠀⠈⠒⠒⠊⠀⠀⠀⠈⠂⠭⠭⠞\n");
 
 }
+/// @brief Draws the monalisa
+void monalisa()
+{
+    printf("         o8%8888,\n");
+    printf("      o88%8888888.                     \n");
+    printf("     8'-    -:8888b                    \n");
+    printf("    8'         8888                   \n");
+    printf("   d8.-=. ,==-.:888b                  \n");
+    printf("   >8 `~` :`~' d8888                  \n");
+    printf("   88         ,88888                 \n");
+    printf("   88b. `-~  ':88888                   \n");
+    printf("   888b v=v~ .:88888                    \n");
+    printf("   88888o--:':::8888                    \n");
+    printf("    `88888| :::' 8888b                      \n");
+    printf("    8888^^'       8888b              \n");
+    printf("    d888           ,%888b.            \n");
+    printf("    d88%            ,?888--'-.         \n");
+    printf(" /88:.__ ,       _%-' ---  -         \n");
+    printf("     '''::===..-'   =  --.  `");
+}
 
 void draw(const char* s) {
 
@@ -74,4 +94,5 @@ void draw(const char* s) {
     if(strcmp(s, "cat") == 0) Cat();
     if(strcmp(s, "lamp") == 0) lamp();
     if(strcmp(s, "nap") == 0) forzaNapoli();
+    if(!strcmp(s, "monalisa")) monalisa(); 
 }
