089fa8570b77203bfe0ba7b9468eb9dae0121b9c
Alex <alexandru.chindris.buisness@gmail.com>
2025-10-01 16:05:58 +0200

    Bug fix

--- asciiart.h
+++ asciiart.h
@@ -2,24 +2,17 @@
 #include <string.h>
 
 /// @brief Disegna il logo della JUVE
-void forzaJuve() {
-    printf("████████████████\n");
-    printf("█▒▒▒▒▒▒▒▒▒██▒▒▒█\n");
-    printf("█▒▒▒▒▒▒▒▒▒██▒▒▒█\n");
-    printf("███████▒▒▒██▒▒▒█\n");
-    printf("███████▒▒▒██▒▒▒█\n");
-    printf("███████▒▒▒██▒▒▒█\n");
-    printf("███████▒▒▒██▒▒▒█\n");
-    printf("███████▒▒▒██▒▒▒█\n");
-    printf("██████▒▒▒▒██▒▒▒█\n");
-    printf("█████▒▒▒▒███▒▒▒█\n");
-    printf("██▀▒▒▒▒▒███▀▒▒▒█\n");
-    printf("██▒▒▒▒▄██▀▒▒▒▒██\n");
-    printf("███████▀▒▒▒▒▄███\n");
-    printf("█████▒▒▒▒▒▄█████\n");
-    printf("████████████████\n");
+void forzaNapoli() {
+    printf("███     ███\n");
+    printf("████    ███\n");
+    printf("██ ██   ███\n");
+    printf("██  ██  ███\n");
+    printf("██   ██ ███\n");
+    printf("██    █████\n");
+    printf("██     ████\n");
+    printf("██      ███\n");
 }
 
 void draw(const char* s) {
-    if(strcmp(s, "juve") == 0) forzaJuve();
+    if(strcmp(s, "nap") == 0) forzaNapoli();
 }
