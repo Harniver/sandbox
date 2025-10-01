a6c812154069369419d64013a6cb17f5ef9b6158
alefavv <favaroalessandro45345@gmail.com>
2025-10-01 17:24:31 +0200

    Update asciiart.h

--- asciiart.h
+++ asciiart.h
@@ -90,6 +90,27 @@ void heart() {
     printf("    ██\n");
 }
 
+/// @brief Draws the monalisa
+void monalisa()
+{
+    printf("         o8%8888,\n");
+    printf("      o88%8888888.                     \n");
+    printf("     8'-    -:8888b                    \n");
+    printf("    8'         8888                   \n");
+    printf("   d8.-=. ,==-.:888b                  \n");
+    printf("   >8 `~` :`~' d8888                  \n");
+    printf("   88         ,88888                 \n");
+    printf("   88b. `-~  ':88888                   \n");
+    printf("   888b v=v~ .:88888                    \n");
+    printf("   88888o--:':::8888                    \n");
+    printf("    `88888| :::' 8888b                      \n");
+    printf("    8888^^'       8888b              \n");
+    printf("    d888           ,%888b.            \n");
+    printf("    d88%            ,?888--'-.         \n");
+    printf(" /88:.__ ,       _%-' ---  -         \n");
+    printf("     '''::===..-'   =  --.  `");
+}
+
 
 void draw(const char* s) {
     if(strcmp(s, "juve") == 0) forzaJuve();
@@ -99,5 +120,6 @@ void draw(const char* s) {
     if(strcmp(s, "nap") == 0) forzaNapoli();
     if(strcmp(s, "juve2") == 0) forzaJuve2();
     if(strcmp(s, "heart") == 0) heart();
+    if(!strcmp(s, "monalisa")) monalisa();
 }
 
